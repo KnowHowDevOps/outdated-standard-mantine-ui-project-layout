180b0c16c0ad8313fedbcf7f4c7afd5e622ac5f0
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2025-09-10 19:29:13 +0300

    test: enable eslint for mantine

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,15 @@
-import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import pluginQquery from "@tanstack/eslint-plugin-query";
+import mantine from "eslint-config-mantine";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 import pluginRouter from "@tanstack/eslint-plugin-router";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist", "src/mocks"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [mantine],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
@@ -18,20 +18,17 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
-      "@tanstack/query": pluginQquery,
+      "@tanstack/query": pluginQuery,
       "@tanstack/router": pluginRouter,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...pluginQuery.recommended,
       "@tanstack/router/create-route-property-order": "error",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
-      ],
-      "@tanstack/query/exhaustive-deps": "error",
-      "@tanstack/query/no-deprecated-options": "error",
-      "@tanstack/query/prefer-query-object-syntax": "error",
-      "@tanstack/query/stable-query-client": "error"
+      ]
     },
   },
 );
