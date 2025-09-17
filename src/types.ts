43815a60b074fb7466ab37442e0a3957be707b3d
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2025-09-17 16:44:13 +0300

    feat: create initial interfaces and api clients for boilerplate

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import { SupportedLocales } from "@/locales.ts";
+
 export type ConfigKeys = "VITE_API_URL_SERVER";
 
 export type IdParam = string | undefined | number;
@@ -34,4 +36,50 @@ export interface GenericPaginatedResponse<T> {
 export interface SortableItem {
   id: IdParam;
   order: number;
+}
+
+export interface User {
+  id?: IdParam;
+  account_id?: IdParam;
+  first_name: string;
+  last_name: string;
+  full_name: string;
+  email: string;
+  timezone?: string;
+  password?: string;
+  is_email_verified?: boolean;
+  has_pending_email_change?: boolean;
+  enforce_email_confirmation_during_registration?: boolean;
+  pending_email?: string;
+  last_login_at?: string;
+  status?: "ACTIVE" | "INACTIVE";
+  role?: "ADMIN";
+  is_account_owner?: boolean;
+  locale?: SupportedLocales;
+}
+
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  token?: string;
+  token_type: string;
+  expires_in: number;
+  user: User;
+}
+
+export interface RegisterAccountRequest {
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+  locale: SupportedLocales;
+}
+
+export interface ResetPasswordRequest {
+  password: string;
+  password_confirmation: string;
 }
