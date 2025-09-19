04e929445a5bdabd4ad7b05bbab83db0f7b0c440
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2025-09-19 17:18:06 +0300

    fix: check process is not undefined

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 
 const TanStackRouterDevtools =
-  process.env.NODE_ENV === "production"
+  typeof process != "undefined" && process.env.NODE_ENV === "production"
     ? () => null // Render nothing in production
     : React.lazy(() =>
         // Lazy load in development
