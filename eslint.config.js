10e93a45d5dc308940f0cda61abeee166a59de68
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2025-09-22 18:17:23 +0300

    chore: migrate to pnpm v10.17.1

--- eslint.config.js
+++ eslint.config.js
@@ -28,6 +28,7 @@ export default tseslint.config(
       ...pluginQuery.recommended,
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
+      "no-console": "off",
       "@tanstack/router/create-route-property-order": "error",
       "react-refresh/only-export-components": [
         "warn",
