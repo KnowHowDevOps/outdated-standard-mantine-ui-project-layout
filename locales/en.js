7f902e04201e32a84451bd95b752108c8b23b53f
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2025-09-25 13:32:55 +0300

    chore: update en locale

--- locales/en.js
+++ locales/en.js
@@ -1,5 +1,1 @@
-/*eslint-disable*/ module.exports = {
-  messages: JSON.parse(
-    '{"7VpPHA":["Confirm"],"CMIzqg":["Are you sure you want to proceed?"],"J/hVSQ":[["0"]],"dEgA5A":["Cancel"],"f7Z8t7":["Confirm Action"]}'
-  ),
-};
+/*eslint-disable*/module.exports={messages:JSON.parse("{\"7VpPHA\":[\"Confirm\"],\"CMIzqg\":[\"Are you sure you want to proceed?\"],\"J/hVSQ\":[[\"0\"]],\"dEgA5A\":[\"Cancel\"],\"f7Z8t7\":[\"Confirm Action\"]}")};
